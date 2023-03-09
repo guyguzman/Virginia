ced3af5de8545b36ef6b1167d8fdeca809887a80
guyguzman <guy.guzman.public@gmail.com>
2023-03-08 16:56:31 -0800

    Initial Commit

---
+++ src/js/common.js
@@ -0,0 +1,21 @@
+  window.onload = function() {
+      showWidthHeight();
+
+      window.addEventListener("resize", () => {
+          showWidthHeight();
+      });
+  };
+
+  function showWidthHeight() {
+      let windowInnerWidth = window.innerWidth;
+      let windowInnerHeight = window.innerHeight;
+      let elementClientWidth = document.documentElement.clientWidth;
+      let elementClientHeight = document.documentElement.clientHeight;
+      let elementOffsetWidth = document.documentElement.offsetWidth;
+      let elementOffsetHeight = document.documentElement.offsetHeight;
+
+      let vh = windowInnerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+      let vw = elementClientWidth * 0.01;
+      document.documentElement.style.setProperty("--vw", `${vw}px`);
+  }
